2edf73a04d0b6189e0a6b74a936224a4c9cfb33a
AllisVain1 <ejikewonderful@gmail.com>
2025-10-31 03:29:11 +0100

    Modify token name, symbol, and add minting functions
    Updated token name and symbol in the constructor. Added minting functionality for the contract deployer and a public mint function for the owner.

--- SimpleToken.sol
+++ SimpleToken.sol
@@ -5,5 +5,16 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
 
 contract MyToken is ERC20, ERC20Permit {
-    constructor() ERC20("MyToken", "MTK") ERC20Permit("MyToken") {}
+    constructor() ERC20("Mini Token", "MINI") ERC20Permit("MiniToken") {}
+
+
+// Minting 1000000 tokens to the contract deployer on deployment
+        _mint(msg.sender, 1000000 * (10 ** 18));
+
+    }
+
+// public mint function for only the owner to call
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
 }
